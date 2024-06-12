7734605044a96456b7f62616d21c460d7e893172
pradyotsrivastava <pradyotsrivastava93@gmail.com>
2024-06-12 21:04:18 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
   //  Armed with an optimistic outlook and a relentless growth mindset, I stand prepared to create a significant impact and accomplish remarkable feats.",
   github: "https://github.com/pradyotsrivastava",
   resume:
-    "https://drive.google.com/file/d/1gD4qioLmoMTMKBh9WEGHpe2GA9MATGWn/view?usp=drive_link",
+    "https://drive.google.com/file/d/1iDzwXLO84fPFEWsr5SIXK0cIgD2SOlz-/view",
   linkedin: "https://www.linkedin.com/in/pradyotsrivastava/",
   twitter: "https://twitter.com/pradyotsrivastava",
   insta: "https://www.instagram.com/pradyot_srivastava/",
